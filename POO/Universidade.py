d5404f63aa280f87f490682480273095236f1832
RicardoSantiago18 <rdssa.snf24@uea.edu.br>
2024-11-17 14:06:27 -0400

    added university class

---
+++ POO/Universidade.py
@@ -0,0 +1,26 @@
+from departamento import Departamento
+
+class Universidade:
+    def __init__(self, nome, endereco, fundacao):
+        self.nome = nome
+        self.endereco = endereco
+        self.fundacao = fundacao
+        self.listaDepartamentos = []  # Associação (1 Universidade contém 1..5 Departamentos)
+
+    def adicionarDepartamento(self, departamento):
+        if len(self.listaDepartamentos) < 5:
+            self.listaDepartamentos.append(departamento)
+        else:
+            print("A universidade já possui o número máximo de departamentos (5).")
+
+    def removerDepartamento(self, departamento):
+        if departamento in self.listaDepartamentos:
+            self.listaDepartamentos.remove(departamento)
+
+    def listarDepartamentos(self):
+        for departamento in self.listaDepartamentos:
+            print(f"Departamento: {departamento.nome}")
+
+    def obterInformacoes(self):
+        return f"Universidade: {self.nome}, Endereço: {self.endereco}, Fundação: {self.fundacao}"
+
